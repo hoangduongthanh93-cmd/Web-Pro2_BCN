fedd9a4109560dc9b7b61257673fc692bbb507a8
embernatsuender-sudo <embernatsuender@gmail.com>
2026-08-13 13:25:14 +0700

    13/8/2026

--- JS/headerDungchung/dungchung.js
+++ JS/headerDungchung/dungchung.js
@@ -1,4 +1,4 @@
-function themHeader() {
+
     const header = document.getElementById("header")
 
     header.innerHTML=`    <div class="top-bar">
@@ -13,7 +13,7 @@ function themHeader() {
             <span></span>
         </button>
 
-        <a href="#" class="logo">CEIN.</a>
+        <a href="../HTML/trangchu.html" class="logo">CEIN.</a>
 
         <nav class="nav-links">
             <a href="../HTML/trangdanhsachSP.html">Shop</a>
@@ -23,7 +23,13 @@ function themHeader() {
         </nav>
 
         <div class="nav-icons">
-            <a href="#" class="icon-link"><img src="../img/iconHeader/MagnifyingGlass.svg" alt="Search"></a>
+            <div class="thanhtimkiem">
+            <input id="NhapvaoTimkiem" type="text" placeholder="Tìm kiếm sản phẩm...">
+            <button type="submit" class="nutTimKiem">
+            <img src="../img/iconHeader/MagnifyingGlass.svg" alt="icon tìm kiếm">
+            </button>
+            <div id="Ketquatimkiem"></div>
+            </div>
             <a href="#" class="icon-link"><img src="../img/iconHeader/UserCircle.svg" alt="User"></a>
             <a href="" class="icon-link" id="theme-toggle"><img src="../img/iconHeader/Heart.svg" alt="Heart"></a>
             <a href="#" class="icon-link"><img src="../img/iconHeader/Bag.svg" alt="Bag"></a>
@@ -31,4 +37,57 @@ function themHeader() {
         </div>
     </header>
 `;
-    }
+document.addEventListener("DOMContentLoaded", () => {
+    const NhapvaoTimkiem = document.getElementById("NhapvaoTimkiem");
+    const Ketquatimkiem = document.getElementById("Ketquatimkiem");
+    let danhSachSanPham = [];
+    fetch("http://localhost:3000/sanpham").then(response => response.json()).then(data => {
+            danhSachSanPham = data;
+        })
+        .catch(error => {
+            console.error("không tải dữ liệu được",error );});
+            NhapvaoTimkiem.addEventListener("input",(event)=>{
+                const Tukhoa=event.target.value.toLowerCase().trim();
+                if (Tukhoa === ""){
+                    Ketquatimkiem.innerHTML="";
+                    Ketquatimkiem.style.display="none";
+                    return null;
+                }
+            
+            const KetquaSP = danhSachSanPham.filter(item=>{
+                const thongtinSp_Name=item.Ten.toLowerCase().trim();
+                const thongtinSp_Description=item.moTa ? item.moTa.toLowerCase().trim() : "";
+                return(thongtinSp_Name.includes(Tukhoa) || thongtinSp_Description.includes(Tukhoa));
+            });
+            Ketquatimkiem.innerHTML="";
+            if(KetquaSP.length===0){
+                Ketquatimkiem.innerHTML=`<div class="KhongCoThongtinTimKiem"><p>Không tìm thấy sản phẩm</p></div>`;
+            }
+            else{
+                KetquaSP.forEach(item =>{
+                    const ThongTinSP_Datimduoc = document.createElement("div");
+                    ThongTinSP_Datimduoc.classList.add("Sanphamtimduoc");
+                    ThongTinSP_Datimduoc.innerHTML=`
+                    <img src="${item.hinhAnh}" alt="${item.Ten}">
+                    <div class="thongTinTimKiem">
+                    <span class="TenSPcanTim">${item.Ten}</span>
+                    <span class ="GiaCuaSP">${Number(item.Gia).toLocaleString("vi-VN")}</span>
+                    </div>
+                    `
+                    Ketquatimkiem.appendChild(ThongTinSP_Datimduoc);
+                });
+            }
+            Ketquatimkiem.style.display="block";
+        });
+            document.addEventListener("click",(event)=> {
+            if(!event.target.closest(".thanhtimkiem")){
+                Ketquatimkiem.style.display="none";}    
+            });
+            Ketquatimkiem.addEventListener("focus",()=>{
+                const Tukhoa = NhapvaoTimkiem.value.trim();
+                if(Tukhoa !== ""){
+                    Ketquatimkiem.style.display="block";
+                }
+            });
+            
+        });
